5352a554cd2d116dcd5d04ecc15a08979c3dd426
haubna <haubner.david@gmail.com>
2023-04-21 21:17:08 +0200

    Update oceans.glsl
    Updated to latest version with foam

--- oceans.glsl
+++ oceans.glsl
@@ -46,8 +46,16 @@ uniform float physics_oceanWaveHorizontalScale;
 uniform vec3 physics_modelOffset;
 // used for offsetting the ripple texture
 uniform float physics_rippleRange;
+// controlling how much foam generates on the ocean
+uniform float physics_foamAmount;
+// controlling the opacity of the foam
+uniform float physics_foamOpacity;
 // texture containing the ripples (basic bump map)
 uniform sampler2D physics_ripples;
+// foam noise
+uniform sampler3D physics_foam;
+// just the generic minecraft lightmap, you can remove this and use the one supplied by Optifine/Iris
+uniform sampler2D physics_lightmap;
 
 // for the vertex shader stage
 out vec3 physics_localPosition;
@@ -114,11 +122,10 @@ vec2 physics_waveDirection(vec2 position, int iterations, float time) {
     return vec2(dx / pow(waveSum, 1.0 - PHYSICS_W_DETAIL));
 }
 
-vec3 physics_waveNormal(vec2 position, float factor, float time) {
-    vec2 wave = -physics_waveDirection(position.xy, physics_iterationsNormal, time);
+vec3 physics_waveNormal(const in vec2 position, const in vec2 direction, const in float factor, const in float time) {
     float oceanHeightFactor = physics_oceanHeight / 13.0;
     float totalFactor = oceanHeightFactor * factor;
-    vec3 waveNormal = normalize(vec3(wave.x * totalFactor, PHYSICS_NORMAL_STRENGTH, wave.y * totalFactor));
+    vec3 waveNormal = normalize(vec3(direction.x * totalFactor, PHYSICS_NORMAL_STRENGTH, direction.y * totalFactor));
     
     vec2 eyePosition = position + physics_modelOffset.xz;
     vec2 rippleFetch = (eyePosition + vec2(physics_rippleRange)) / (physics_rippleRange * 2.0);
@@ -132,9 +139,71 @@ vec3 physics_waveNormal(vec2 position, float factor, float time) {
     float normalx = left - right;
     float normalz = top - bottom;
     vec3 rippleNormal = normalize(vec3(normalx, 1.0, normalz));
-    waveNormal = normalize(mix(waveNormal, rippleNormal, pow(totalEffect, 0.5)));
+    return normalize(mix(waveNormal, rippleNormal, pow(totalEffect, 0.5)));
+}
 
-    return waveNormal;
+struct WavePixelData {
+    vec2 direction;
+    vec2 worldPos;
+    vec3 normal;
+    float foam;
+    float height;
+};
+
+WavePixelData physics_wavePixel(const in vec2 position, const in float factor, const in float iterations, const in float time) {
+    vec2 wavePos = (position.xy - physics_waveOffset) * PHYSICS_XZ_SCALE * physics_oceanWaveHorizontalScale;
+    float iter = 0.0;
+    float frequency = PHYSICS_FREQUENCY;
+    float speed = PHYSICS_SPEED;
+    float weight = 1.0;
+    float height = 0.0;
+    float waveSum = 0.0;
+    float modifiedTime = time * PHYSICS_TIME_MULTIPLICATOR;
+    vec2 dx = vec2(0.0);
+    
+    for (int i = 0; i < iterations; i++) {
+        vec2 direction = vec2(sin(iter), cos(iter));
+        float x = dot(direction, wavePos) * frequency + modifiedTime * speed;
+        float wave = exp(sin(x) - 1.0);
+        float result = wave * cos(x);
+        vec2 force = result * weight * direction;
+        
+        dx += force / pow(weight, PHYSICS_W_DETAIL); 
+        wavePos -= force * PHYSICS_DRAG_MULT;
+        height += wave * weight;
+        iter += PHYSICS_ITER_INC;
+        waveSum += weight;
+        weight *= PHYSICS_WEIGHT;
+        frequency *= PHYSICS_FREQUENCY_MULT;
+        speed *= PHYSICS_SPEED_MULT;
+    }
+    
+    WavePixelData data;
+    data.direction = -vec2(dx / pow(waveSum, 1.0 - PHYSICS_W_DETAIL));
+    data.worldPos = wavePos / physics_oceanWaveHorizontalScale / PHYSICS_XZ_SCALE;
+    data.height = height / waveSum * physics_oceanHeight * factor - physics_oceanHeight * factor * 0.5;
+    
+    data.normal = physics_waveNormal(position, data.direction, factor, time);
+
+    float waveAmplitude = data.height * pow(max(data.normal.y, 0.0), 4.0);
+    vec2 waterUV = mix(position - physics_waveOffset, data.worldPos, clamp(factor * 2.0, 0.2, 1.0));
+    
+    vec2 s1 = textureLod(physics_foam, vec3(waterUV * 0.26, physics_globalTime / 360.0), 0).rg;
+    vec2 s2 = textureLod(physics_foam, vec3(waterUV * 0.02, physics_globalTime / 360.0 + 0.5), 0).rg;
+    vec2 s3 = textureLod(physics_foam, vec3(waterUV * 0.1, physics_globalTime / 360.0 + 1.0), 0).rg;
+    
+    float waterSurfaceNoise = s1.r * s2.r * s3.r * 2.8 * physics_foamAmount;
+    waveAmplitude = clamp(waveAmplitude * 1.2, 0.0, 1.0);
+    waterSurfaceNoise = (1.0 - waveAmplitude) * waterSurfaceNoise + waveAmplitude * physics_foamAmount;
+    
+    float worleyNoise = 0.2 + 0.8 * s1.g * (1.0 - s2.g);
+    float waterFoamMinSmooth = 0.45;
+    float waterFoamMaxSmooth = 2.0;
+    waterSurfaceNoise = smoothstep(waterFoamMinSmooth, 1.0, waterSurfaceNoise) * worleyNoise;
+    
+    data.foam = clamp(waterFoamMaxSmooth * waterSurfaceNoise * physics_foamOpacity, 0.0, 1.0);
+    
+    return data;
 }
 
 // VERTEX STAGE
@@ -151,6 +220,7 @@ void main() {
 
 // FRAGMENT STAGE
 void main() {
-    // normal is in world space
-    vec3 normal = physics_waveNormal(physics_localPosition.xz, physics_localWaviness, physics_gameTime);
+    WavePixelData wave = physics_wavePixel(physics_localPosition.xz, physics_localWaviness, physics_iterationsNormal, physics_gameTime);
+    
+    // access the wave struct data however you want, wave.normal is in world space, wave.foam is the final foam amount
 }
